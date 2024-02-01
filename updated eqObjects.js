53166241c1be60ff10a0085be965738268c3559c
subara7 <149124711+subara7@users.noreply.github.com>
2024-02-01 10:51:09 -0800

    updated eqObjects.js

---
+++ updated eqObjects.js
@@ -0,0 +1,26 @@
+const eqArrays = function(arr1, arr2) {
+
+  };
+  
+  const eqObjects = function(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+  
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (const key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+  
+    return true;
+  };
